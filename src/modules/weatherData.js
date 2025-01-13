de00ec76fa76477fc241849c27fe1fff6c6efd4c
vladh <hadyak305246@gmail.com>
2025-01-13 14:55:34 -0700

    Get weather data forecast for 6 days

--- src/modules/weatherData.js
+++ src/modules/weatherData.js
@@ -1,4 +1,4 @@
-import SunCalc from "suncalc";
+import SunCalc from "suncalc"; // Library for calculating sun's position
 
 const userInput = document.getElementById("search-input");
 const btnRequest = document.getElementById("search-btn");
@@ -7,11 +7,15 @@ const API_KEY = "9545QA2MGPWNHSND234UFU28K"; // Public Visual Crossing API key
 
 const getLocation = () => userInput.value;
 
-async function getWeatherData() {
+// Get weather data from API
+async function fetchWeatherData() {
   const location = getLocation();
-  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&elements=datetime%2Cname%2Caddress%2Clatitude%2Clongitude%2Ctempmax%2Ctempmin%2Ctemp%2Cfeelslike%2Cdew%2Chumidity%2Cprecip%2Cprecipprob%2Cpreciptype%2Csnow%2Csnowdepth%2Cwindspeed%2Cwindspeedmean%2Cwinddir%2Cpressure%2Ccloudcover%2Cvisibility%2Csolarradiation%2Csolarenergy%2Cuvindex%2Csunrise%2Csunset%2Cconditions%2Cdescription%2Csunelevation&key=${API_KEY}&contentType=json`;
-
   if (location.trim() === "") return;
+
+  const url =
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}` +
+    `?unitGroup=metric&elements=datetime%2Cname%2Caddress%2Clatitude%2Clongitude%2Ctempmax%2Ctempmin%2Ctemp%2Cfeelslike%2Cdew%2Chumidity%2Cprecip%2Cprecipprob%2Cpreciptype%2Csnow%2Csnowdepth%2Cwindspeed%2Cwindspeedmean%2Cwinddir%2Cpressure%2Ccloudcover%2Cvisibility%2Csolarradiation%2Csolarenergy%2Cuvindex%2Csunrise%2Csunset%2Cconditions%2Cdescription%2Csunelevation` +
+    `&key=${API_KEY}&contentType=json`;
 
   try {
     const response = await fetch(url, { mode: "cors" });
@@ -22,50 +26,89 @@ async function getWeatherData() {
 
     const data = await response.json();
 
-    processWeatherData(data); // Process json data
-    console.log(data);
+    handleWeatherData(data); // Process json data
+    console.log("JSON Data:", data);
   } catch (err) {
     console.log("Error fetching weather data!", err.message);
   }
 }
 
-// Extract API data into object
-function processWeatherData(data) {
-  // Variables for sun elevation calculation
-  const tzOffset = data.tzoffset;
+// Process and structure raw data from API into objects
+function organizeData(data) {
+  const daysOfWeek = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
+  // Arr of the following 6 days with weather data (excluding Today's day)
+  const weekForecast = data.days.slice(1, 7);
+  const currentConditions = data.currentConditions;
+
+  // Variables for calculating sun elevation
   const currentDate = new Date();
-  const localDate = new Date(currentDate.getTime() + tzOffset * 60000);
+  const localDate = new Date(currentDate.getTime());
   const latitude = data.latitude;
   const longitude = data.longitude;
 
-  // Include data from API that should be displayed
-  const obj = {
+  // Data for today
+  const currentData = {
     location: data.resolvedAddress,
-    temperature: data.currentConditions.temp, // celsius
-    condition: data.currentConditions.conditions,
-    feelsLike: data.currentConditions.feelslike,
-    humidity: data.currentConditions.humidity, // percentage
-    precipitation: data.currentConditions.precip, // millimeters
-    sunrise: data.currentConditions.sunrise,
-    sunset: data.currentConditions.sunset,
-    windSpeed: data.currentConditions.windspeed, // km/h
-    visibility: data.currentConditions.visibility, // km
+    day: "Today",
+    temperature: currentConditions.temp, // celsius
+    condition: currentConditions.conditions,
+    feelsLike: currentConditions.feelslike,
+    humidity: currentConditions.humidity, // percentage
+    precipitation: currentConditions.precip, // millimeters
+    sunrise: currentConditions.sunrise,
+    sunset: currentConditions.sunset,
+    windSpeed: currentConditions.windspeed, // km/h
+    visibility: currentConditions.visibility, // km
 
     get sunElevation() {
       const sunPosition = SunCalc.getPosition(localDate, latitude, longitude);
-      return (sunPosition.altitude * 180) / Math.PI; // degrees
+      return (sunPosition.altitude * 180) / Math.PI; // Convert from radians to degrees
     },
   };
 
-  // function that takes object, and displays the data
-  console.log(obj);
+  // Data for the next 6 days
+  const weekForecastData = weekForecast.map((day) => {
+    const date = new Date(day.datetime); // Convert string date into Date object
+    return {
+      dayX: day.datetime, // !!!
+      day: daysOfWeek[date.getDay()], // Day of the week
+      temperatureMax: day.tempmax,
+      temperatureMin: day.tempmin,
+      condition: day.conditions,
+      feelslike: day.feelslike,
+      humidity: day.humidity,
+      precipitation: day.precip,
+      sunrise: day.sunrise,
+      sunset: day.sunset,
+      windSpeed: day.windspeed,
+      visibility: day.visibility,
+    };
+  });
+  return { currentData, weekForecastData };
 }
 
-function fetchWeatherData() {
+// Destructure processed data into weather data objects for easy access
+function handleWeatherData(data) {
+  // Get today's weather data, 6-day forecast data
+  const { currentData, weekForecastData } = organizeData(data);
+  console.log(currentData);
+  console.log(weekForecastData);
+}
+
+function initFetch() {
   btnRequest.addEventListener("click", (e) => {
     e.preventDefault();
-    getWeatherData();
+    fetchWeatherData();
   });
 }
 
-export { fetchWeatherData };
+export { initFetch };
