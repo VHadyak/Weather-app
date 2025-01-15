61b26bec2f20467bded985575209150bba57ce35
Vlad <hadyak305246@gmail.com>
2025-01-15 03:32:58 -0700

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import globals from "globals";
+import js from "@eslint/js";
+import prettierConfig from "eslint-config-prettier";
+
+export default [
+  {
+    files: ["**/*.js"], // Apply to JavaScript files
+    languageOptions: {
+      globals: {
+        ...globals.browser, // Include browser globals if needed
+        ...globals.node, // Include Node.js globals
+      },
+      parserOptions: {
+        ecmaVersion: "latest", // Use the latest ECMAScript standard
+        sourceType: "module", // Support ES modules
+      },
+    },
+    plugins: {
+      js, // Register the @eslint/js plugin
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...prettierConfig.rules, // Disable ESLint rules that conflict with Prettier
+      "no-unused-vars": "error",
+      "no-undef": "warn",
+      "prefer-arrow-callback": "warn", // Encourage arrow functions in callbacks
+    },
+  },
+];
---
+++ webpack.common.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          process.env.NODE_ENV === "production"
+            ? MiniCssExtractPlugin.loader
+            : "style-loader",
+          "css-loader",
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(js|mjs|cjs)$/,
+        exclude: /node_modules/,  // Exclude node_modules
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              ["@babel/preset-env", { targets: "IE 11" }]
+            ],
+            cacheDirectory: true,  // Enable caching for faster builds
+          },
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+  },
+};
